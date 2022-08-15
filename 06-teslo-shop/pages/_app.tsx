8f95d41df5396f75a0adaf3a312a589d102148bb
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2022-08-14 21:14:07 -0600

    Paypal payments

--- 06-teslo-shop/pages/_app.tsx
+++ 06-teslo-shop/pages/_app.tsx
@@ -8,27 +8,31 @@ import { UIProvider } from '../context/ui/UIProvider';
 import { CartProvider } from '../context/cart/CartProvider';
 import { AuthProvider } from '../context/auth/AuthProvider';
 import { SessionProvider } from "next-auth/react"
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider>
-      <SWRConfig 
-        value={{
-          // refreshInterval: 3000,
-          fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-        }}
-      >
-        <AuthProvider>
-          <CartProvider>
-            <UIProvider>
-              <ThemeProvider theme={lightTheme}>
-                <CssBaseline />
-                <Component {...pageProps} />
-              </ThemeProvider>
-            </UIProvider>
-          </CartProvider>
-        </AuthProvider>
-      </SWRConfig>
+      <PayPalScriptProvider options={{ "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || '' }}>
+        <SWRConfig 
+          value={{
+            // refreshInterval: 3000,
+            fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+          }}
+        >
+          <AuthProvider>
+            <CartProvider>
+              <UIProvider>
+                <ThemeProvider theme={lightTheme}>
+                  <CssBaseline />
+                  <Component {...pageProps} />
+                </ThemeProvider>
+              </UIProvider>
+            </CartProvider>
+          </AuthProvider>
+        </SWRConfig>
+      </PayPalScriptProvider>
     </SessionProvider>
   )
 }
--- 06-teslo-shop/pages/orders/[id].tsx
+++ 06-teslo-shop/pages/orders/[id].tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Grid, Typography, Divider, Button, Link, Chip } from '@mui/material';
+import { Card, CardContent, Grid, Typography, Divider, Button, Link, Chip, CircularProgress } from '@mui/material';
 import { Box } from '@mui/system';
 import { CartList } from '../../components/cart/CartList';
 import { OrderSummary } from '../../components/cart/OrderSummary';
@@ -10,14 +10,56 @@ import { getSession } from 'next-auth/react';
 import { getOrderById } from '../../database/dbOrders';
 import { IOrder } from '../../interfaces/order';
 import { countries } from '../../utils/countries';
+import { PayPalButtons } from "@paypal/react-paypal-js";
+import tesloApi from '../../api/tesloApi';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+
 
 interface Props {
     order: IOrder
 }
 
+export type OrderResponseBody = {
+    id: string;
+    status:
+        | "COMPLETED"
+        | "SAVED"
+        | "APPROVED"
+        | "VOIDED"
+        | "PAYER_ACTION_REQUIRED";
+}
+
 const OrderPage: NextPage<Props> = ({order}) => {
     
+    const router = useRouter();
     const {shippingAddress} = order;
+
+    const [isPaying, setIsPaying] = useState(false);
+
+    const onOrderCompleted = async (details: OrderResponseBody) => {
+        
+        if(details.status !== 'COMPLETED'){
+            return alert('No hay pago en Paypal');
+        }
+
+        setIsPaying(true);
+
+        try {
+
+            const {data} = await tesloApi.post(`/orders/pay`, {
+                transactionId: details.id,
+                orderId: order._id
+            });
+
+            router.reload();
+            
+        } catch (error) {
+            setIsPaying(false);
+            console.log(error);
+            alert('Error');
+        }
+    }
 
   return (
     <ShopLayout title='Resumen de orden' pageDescription='Resumen de la orden'>
@@ -57,13 +99,43 @@ const OrderPage: NextPage<Props> = ({order}) => {
                         <OrderSummary order={order} />
 
                         <Box sx={{mt: 3}} display='flex' flexDirection='column'>
-                            {
-                                order.isPaid ? (
-                                    <Chip sx={{my:2}} label="Orden ya fue pagada" variant="outlined" color="success" icon={<CreditScoreOutlined />} />
-                                ) : (
-                                    <h1>Pagar</h1>
-                                )
-                            }
+
+                            <Box display='flex' justifyContent='center' className='fadeIn' sx={{display: isPaying ? 'flex' : 'none'}}>
+                                <CircularProgress />
+                            </Box>
+
+                            <Box display='flex' flexDirection='column' justifyContent='center' className='fadeIn' sx={{display: isPaying ? 'none' : 'flex', flex: 1}}>
+                                {
+                                    order.isPaid ? (
+                                        <Chip sx={{my:2}} label="Orden ya fue pagada" variant="outlined" color="success" icon={<CreditScoreOutlined />} />
+                                    ) : (
+                                        <PayPalButtons
+                                            createOrder={(data, actions) => {
+                                                return actions.order.create({
+                                                    purchase_units: [
+                                                        {
+                                                            amount: {
+                                                                value: order.total.toString(),
+                                                            },
+                                                        },
+                                                    ],
+                                                });
+                                            }}
+                                            onApprove={(data, actions) => {
+                                                return actions.order!.capture().then((details) => {
+                                                    
+                                                    onOrderCompleted(details);
+                                                    //console.log({details});
+                                                    //const name = details.payer.name?.given_name;
+                                                    //alert(`Transaction completed by ${name}`);
+                                                });
+                                            }}
+                                        />
+                                    )
+                                }
+                            </Box>
+
+                            
                         </Box>
                     </CardContent>
                 </Card>
